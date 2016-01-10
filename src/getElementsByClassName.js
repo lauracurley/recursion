5cf9c65d7e90e285d9855bf2ad3f0a2774daa373
Laura Curley <info@lauracurley.com>
2016-01-10 13:48:29 -0500

    Solve recursion

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,35 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+var stack = [];
+var getElementsByClassName = function(className, element){
+  	
+  	// The element being checked on the first iteration is document.body
+  	var e = element || document.body;
+
+  	// Reset the stack variable on the first iteration through the dom
+   	if (e === document.body){
+   		stack = [];
+   	}
+
+   	// If the element has a classlist that contains the search parameter,
+   	// push it to the stack
+   	if (e.classList){
+	   	if ( _.contains(e.classList, className) ) {
+	   		if (stack.indexOf(e) < 0){
+	   			stack.push(e);
+	   		} 
+		}
+	}
+
+	/* Keep searching for childNodes on each element
+	*  and execute the recursive case until there are 
+	*  more childNodes
+	*/
+   	if (e.childNodes){
+   		_.each(e.childNodes, function(item){
+   			getElementsByClassName(className, item);
+   		});
+   	}
+	return stack;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,44 @@
 
 // but you don't so you're going to write it from scratch:
 
-var stringifyJSON = function(obj) {
-  // your code goes here
+var stringifyJSON = function(obj, array) {
+  var result = '';
+  if (typeof obj === 'number'){
+  	return result + obj;
+  } else if ( obj === null){
+  	return 'null';
+  } else if (typeof obj === 'boolean'){
+  	return result + obj.toString();
+  } else if (typeof obj === 'string'){
+  	return '\"' + obj.toString() + '\"';
+  } else if (typeof obj === 'undefined' || typeof obj === 'function'){
+  	return result + '';
+  } else if (Array.isArray(obj)){
+  	var baseArray = array || [];
+  	// Base Case:  obj.length === 0
+  	if (obj.length === 0){
+  		if (baseArray.length === 0){
+  			return '[]';
+  		} 
+  	} else {
+  		baseArray.push( stringifyJSON(obj.shift()) );
+  		stringifyJSON(obj, baseArray);
+  	}
+  	result += "\[" + baseArray.join(",") + "\]";
+  } else if (typeof obj === 'object'){
+  		if (Object.keys(obj).length === 0){
+  			return '{}';
+  		}
+  		result += '\{';
+  		for (var key in obj){
+  			if (typeof obj[key] === 'function' || obj[key] === undefined){
+  				result += '';
+  			} else {
+  				result += stringifyJSON(key) + ":" + stringifyJSON(obj[key]) + ",";
+  			}	
+  		}
+  		result = result.substring(0, result.length -1);
+  		result.length > 1 ? result += '\}' : result = '{}';
+  } 
+  return result;
 };
